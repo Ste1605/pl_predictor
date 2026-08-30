4687c0483168e92b7665def8b3ce8c99c54f46dd
UCDPA_Ste <40280202+Ste1605@users.noreply.github.com>
2026-08-30 08:08:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ def show_confirmation_modal(user_name, predictions_dict, selected_gw):
         })
     
     st.dataframe(pd.DataFrame(summary_data), hide_index=True, use_container_width=True)
-    st.success("Your picks are safely locked in Google Sheets!")
+    st.success("Your picks are safely locked in - Good Luck!")
     if st.button("Close", use_container_width=True):
         st.rerun()
 
